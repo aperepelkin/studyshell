9d00788c98ef5c7906a7cde9687645ff0ea1dfc0
Student <Student>
2017-07-18 19:48:16 +0300

    Initial Commit

---
+++ src/main/java/ru/itstep/studyshell/App.java
@@ -0,0 +1,13 @@
+package ru.itstep.studyshell;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
