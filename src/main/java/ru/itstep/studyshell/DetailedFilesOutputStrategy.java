7ff84b658aafcc7c4621aadbc9c94263e98f66f1
Student <Student@C14>
2017-07-25 21:59:41 +0300

    update to classwork 25/07/2017

--- src/main/java/ru/itstep/studyshell/DetailedFilesOutputStrategy.java
+++ src/main/java/ru/itstep/studyshell/DetailedFilesOutputStrategy.java
@@ -2,26 +2,81 @@ package ru.itstep.studyshell;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+
+import javax.swing.plaf.metal.MetalIconFactory.FolderIcon16;
 
 public class DetailedFilesOutputStrategy implements FilesOutputStrategy {
 
 	public void output(List<File> files) {
-		
-		for( File f: files) {
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-			Long space = f.getTotalSpace();
-			double spaceKb = space/1000;
-			
-			System.out.println("Последний Формат : " + sdf.format(f.lastModified()));
-			System.out.println("Файл: " + f.isFile());
-			System.out.println("Каталог: "+f.isDirectory());
-			System.out.println(String.format("%d", )"Размер: " + spaceKb + "-Kb");
-			System.out.println("Имя" +f.getName());
+
+		long max = 0;
+		for (File f : files) {
+			if (f.length() > max)
+				max = f.length();
 		}
-		
+
+		System.out.println(max);
+		String aString = Long.toString(max);
+
+		for (File f : files) {
+
+			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+			double length = f.length() / 1024.0;
+
+			String type = null;
+			if (f.isFile())
+				type = "Файл ";
+			if (f.isDirectory())
+				type = "Каталог ";
+
+			String line = String.format("%s %8s %" + (aString.length() + 5) + ".4fkb %s ", sdf.format(f.lastModified()),
+					type, length, f.getName());
+
+			System.out.println(line);
+		}
+
+		System.out.println("Размер папки: " + folderSize(files));
+
 	}
 
+	private long folderSize(List<File> files) {
+
+		long foldersize = 0;
+
+		for (File f : files) {
+			if (f.isFile())
+				foldersize += f.length();
+
+			if (f.isDirectory()) {
+				File[] list = f.listFiles();
+				if (list != null)
+					foldersize += folderSize(Arrays.asList(list));
+			}
+
+		}
+
+		return foldersize;
+	}
+/*
+	private long countFiles(File file2) {
+		if (.isDirectory()) {
+			File[] files = dir.listFiles();
+			for (File file : files) {
+				if (file.isDirectory())
+					countFiles(file);
+			}
+			for (File file : files) {
+				int n = 0;
+				if (file.isFile()) {
+					n++;
+
+					return 0;
+				}
+			}
+		}
+	}
+	*/
 }
