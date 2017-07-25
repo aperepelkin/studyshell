c5a705eadd80a6581c88c6bb9ca0f13b3464bd87
zotch <zotch@DESKTOP-88S5ORQ>
2017-07-25 13:03:27 +0300

    uyhi

---
+++ src/main/DetailedFilesOutputStrategy.java
@@ -0,0 +1,10 @@
+package ru.itstep.studyshell;
+
+import java.io.File;
+import java.util.List;
+
+public class DetailedFilesOutputStrategy implements FilesOutputStrategy {
+
+	public void output(List<File> files) {
+	}
+}
