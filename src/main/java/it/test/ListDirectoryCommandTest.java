506c3b9dd2fb77ee0fe23d5101cf1ffae1f7e910
Student <Student@6-2-1>
2017-08-01 19:48:43 +0300

    21

---
+++ src/main/java/it/test/ListDirectoryCommandTest.java
@@ -0,0 +1,21 @@
+package it.test;
+
+import org.junit.Test;
+
+import ru.itstep.studyshell.Command;
+import ru.itstep.studyshell.CommandFactory;
+import ru.itstep.studyshell.ListDirectoryCommand;
+import ru.itstep.studyshell.ListDirectoryCommandFactory;
+
+public class ListDirectoryCommandTest {
+
+	@Test
+	public void TestValid() {
+		ListDirectoryCommandTest command = new ListDirectoryCommandTest();
+	}
+	@Test
+	public void testValidCommandFactory() {
+		
+	}
+}
+
